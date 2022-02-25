1968a18ec80049d709dd214f217ca9d5cb05c582
ameer <ameermohammadramzani@gmail.com>
2022-02-25 18:10:13 +0330

    Project done

--- Produck/ViewController.swift
+++ Produck/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var start_finish_button: UIButton!
+    @IBOutlet weak var status_label: UILabel!
+    var x = 0
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func start_stop_tapped(_ sender: UIButton) {
+        let defaults = UserDefaults.standard
+        if x % 2 == 0 {
+            sender.setTitle("Done", for: .normal)
+            defaults.set(Date(), forKey: "start_time")
+            x += 1
+        } else {
+            sender.setTitle("Start", for: .normal)
+            let time_set = defaults.object(forKey: "start_time") as! Date
 
+            let formattter = RelativeDateTimeFormatter()
+            formattter.unitsStyle = .full
+            
+            status_label.text = "You worked \(formattter.localizedString(for: Date(), relativeTo: time_set))"
+            x -= 1
+        }
+        
+    }
+    
 }
 
